656d4017a80a6827620654af9f85f5fc2f08944d
Jônatas da Silva Gonçalves <145167220+JnSGoncalves@users.noreply.github.com>
2025-04-26 15:06:52 -0300

    add: conexão com a wit.ai

--- FuriaChatBotApi/Program.cs
+++ FuriaChatBotApi/Program.cs
@@ -4,19 +4,12 @@ using FuriaChatBotApi.Interface;
 using FuriaChatBotApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Carrega variáveis do .env
 Env.Load();
 
-// Configurações manuais (simples e seguras)
 builder.Services.Configure<GeminiSettings>(options => {
     options.ApiKey = Environment.GetEnvironmentVariable("GEMINI_API_KEY");
     options.Endpoint = Environment.GetEnvironmentVariable("GEMINI_API_ENDPOINT");
@@ -24,6 +17,7 @@ builder.Services.Configure<GeminiSettings>(options => {
 
 // Serviços da API
 builder.Services.AddHttpClient<ILLMService, LLMService>();
+builder.Services.AddHttpClient<WitAiService, WitAiService>();
 builder.Services.AddScoped<IChatService, ChatService>();
 
 builder.Services.AddControllers();
--- FuriaChatBotApi/Services/ChatService.cs
+++ FuriaChatBotApi/Services/ChatService.cs
@@ -4,12 +4,16 @@ using FuriaChatBotApi.Model;
 namespace FuriaChatBotApi.Services {
     public class ChatService : IChatService{
         private readonly ILLMService _llmService;
+        private readonly WitAiService _witAiService;
 
-        public ChatService(ILLMService llmService) {
+        public ChatService(ILLMService llmService, WitAiService witAiService) {
             _llmService = llmService;
+            _witAiService = witAiService;
         }
 
         public async Task<ChatResponse> GetResponseAsync(string message) {
+            string? nlpResponse = await _witAiService.GetWitResponseAsync(message);
+
             string? answer = await _llmService.GetResponseAsync(message);
 
             if(answer == null) { 
